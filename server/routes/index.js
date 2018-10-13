476638c1681f26502b42e5b70458e65484682f6c
pputra <mynameisyoga@live.com>
2018-10-13 21:28:18 +0700

    add img upload API

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,7 @@ const ItemControler = require('../controllers/itemController');
 const Middlewares = require('../middlewares/index');
 const CategortController = require('../controllers/categoryController');
 const TransactionController = require('../controllers/transactionController');
+images = require('../helpers/images')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -48,6 +49,17 @@ router.patch('/cart/:id', Middlewares.isLogin, CartController.removeItem);
 router.post('/item', ItemControler.create); 
 
 router.post('/cart', CartController.create);
+
+router.post('/item/upload',
+  images.multer.single('image'), 
+  images.sendUploadToGCS,
+  (req, res) => {
+    res.send({
+      status: 200,
+      message: 'Your file is successfully uploaded',
+      link: req.file.cloudStoragePublicUrl
+    })
+  })
 
 
 
