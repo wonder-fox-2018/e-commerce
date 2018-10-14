a4b3c44226e0d7e0af6747547f6a3e48da81f28b
pputra <mynameisyoga@live.com>
2018-10-14 22:34:55 +0700

    finish refactor navbar and footer components

---
+++ client/components/footer-components/index.js
@@ -0,0 +1,18 @@
+Vue.component('footer-components', {
+    template: `
+    
+    <footer class="bg-dark" id="footer">
+            <div class="flex-container">
+                Designed by: Payoga Putra
+                <div class="social-media">
+                    <i class="fab fa-github"></i>
+                    <i class="fab fa-linkedin"></i>
+                    <i class="far fa-envelope"></i>
+                </div>
+            </div>
+    </footer>
+    
+    
+    
+    `
+})
