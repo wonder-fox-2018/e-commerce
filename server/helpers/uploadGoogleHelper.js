d96e37604a851c7486a92b07c3687d5769b67503
esadeli <efrat.sadeli@gmail.com>
2018-10-14 15:25:26 +0700

    Add File Type for Upload Validation Server OK

--- server/helpers/uploadGoogleHelper.js
+++ server/helpers/uploadGoogleHelper.js
@@ -49,13 +49,32 @@ const sendUploadToGCS = (req,res,next) =>{
    stream.end(req.file.buffer)
 }
 
+// const Multer = require('multer'),
+//       multer = Multer({
+//         storage: Multer.MemoryStorage,
+//         limits: {
+//           fileSize: 5 * 1024 * 1024
+//         }
+//         // dest: '../images'
+// })
+
 const Multer = require('multer'),
       multer = Multer({
+        fileFilter: function(req,file,cb){
+            // console.log('FILE-----', file.mimetype)
+            // console.log('Type of-----', typeof file.mimetype)
+
+            if(file.mimetype == 'image/jpg' || file.mimetype == 'image/jpeg' || file.mimetype == 'image/png'){
+                
+                cb(null,true)
+            }else{
+                cb(new Error('Only .jpg, .jpeg, and .png files allowed'))
+            }
+        },  
         storage: Multer.MemoryStorage,
         limits: {
           fileSize: 5 * 1024 * 1024
         }
-        // dest: '../images'
 })
 
 module.exports = {
